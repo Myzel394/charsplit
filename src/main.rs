555aa7bfa4aba13f1aee9fb184a9fddeecdc3f51
Myzel394 <50424412+Myzel394@users.noreply.github.com>
2024-06-08 20:05:37 +0200

    feat: Pretty print output using prettytable

--- src/main.rs
+++ src/main.rs
@@ -1,63 +1,13 @@
 use core::fmt;
 use std::{char, io::{self, Read}, iter::Iterator, slice::Iter};
 use colored::Colorize;
+use config::config::Config;
+use charset::charset::{Charset, Utf8Charset};
+use prettytable::{format, row, Table};
 
 mod config;
 mod charset;
 mod utf8_groups;
-
-use config::config::Config;
-use charset::charset::{Charset, Utf8Charset};
-
-
-macro_rules! format_num_as_byte {
-    ($a:expr) => {
-        format!("{:08b}", $a)
-    };
-}
-
-macro_rules! format_first_grapheme_byte {
-    ($byte:expr, $character:expr, $type: expr, $truecolor:expr, $utf8:expr) => {
-        format!(
-            "{0}\t{1} ({2}; {3}, 0x{3:X})\t=\t{4}\t{5}\n",
-            $character.to_string().yellow(),
-            (|| {
-                let text = format_num_as_byte!($byte);
-
-                if ($truecolor) {
-                    // Use terminal color   
-                    return text.truecolor(128, 128, 128); 
-                } else {
-                    return text.bright_green();
-                }
-            })(),
-            format!("{:03}", $byte).bright_blue(),
-            (*$character) as u32,
-            $type,
-            if (*$utf8 == 0) { "".to_string() } else { Utf8Charset::get_description($utf8) },
-        )
-    };
-}
-
-macro_rules! format_grapheme_byte {
-    ($byte:expr, $type: expr, $truecolor:expr) => {
-        format!(
-            " \t{} ({})\t=\t{}\t \n",
-            (|| {
-                let text = format_num_as_byte!($byte);
-
-                if ($truecolor) {
-                    // Use terminal color   
-                    return text.truecolor(128, 128, 128); 
-                } else {
-                    return text.bright_green();
-                }
-            })(),
-            format!("{:03}", $byte).bright_blue(),
-            $type,
-        )
-    };
-}
 
 #[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Hash, Clone, Copy)]
 enum ByteType {
@@ -94,34 +44,35 @@ fn get_byte_type(byte: &u8) -> ByteType {
     ByteType::Unknown
 }
 
-fn format_grapheme(grapheme: &char, use_truecolors: &bool) -> String {
+fn format_grapheme(table: &mut Table, grapheme: &char, use_truecolors: &bool) {
     let grapheme_string = grapheme.to_string();
     let bytes = grapheme_string.as_bytes();
 
-    let mut string = "".to_string();
-    let first_byte = &bytes.get(0).unwrap();
+    let first_byte = bytes.get(0).unwrap();
 
-    string.push_str(
-        &format_first_grapheme_byte!(
-            &first_byte,
-            grapheme,
-            get_byte_type(&first_byte),
-            *use_truecolors,
-            &(*grapheme as u32)
-        )
-    );
+    table.add_row(row![
+        grapheme,
+        format!("{:0b}", first_byte),
+        format!("{}", (*first_byte) as u32),
+        (*grapheme) as u32,
+        format!("0x{:X}", (*grapheme) as u32),
+        get_byte_type(&first_byte),
+        Utf8Charset::get_description(&((*grapheme) as u32)),
+    ]);
+
 
     for byte in &bytes[1..] {
-        string.push_str(
-            &format_grapheme_byte!(
-                byte,
-                get_byte_type(&byte),
-                *use_truecolors
-            )
-        );
+        table.add_row(row![
+            " ",
+            format!("{:08b}", byte),
+            format!("{}", (*byte) as u32),
+            " ",
+            " ",
+            get_byte_type(&byte),
+            " ",
+        ]);
     }
 
-    string
 }
 
 fn main() {
@@ -134,20 +85,25 @@ fn main() {
     };
     let graphemes = input.chars();
 
-    for grapheme in graphemes {
-        let output = format_grapheme(&grapheme, &config.use_truecolor);
+    let mut table = Table::new();
+    let format = format::FormatBuilder::new()
+        .padding(1, 1)
+        .build();
+    table.set_format(format);
 
-        println!("{}", output);
+    for grapheme in graphemes {
+        format_grapheme(&mut table, &grapheme, &config.use_truecolor);
     }
 
-    // let bytes = input.as_bytes();
-    // let formatted_bytes: Vec<String> = bytes.iter().map(|byte| format_num_as_byte(&byte)).collect();
-    //
-    // println!("{:?}", &formatted_bytes);
-    // Split per 8 bits
-    // let byte_indexes: Vec<u8> = utf8_indexes.flat_map(|index| split_into_bytes(&index)).collect();
-    // dbg!(&byte_indexes);
-    //
-    // // show in binary notation
-    // println!("{:?}", byte_indexes.iter().map(|index| format_num_as_byte(&index)).collect::<Vec<String>>());
+    table.set_titles(row![
+        "Grapheme",
+        "Byte in binary",
+        "Byte in decimal",
+        "Unicode in decimal",
+        "Unicode in hex",
+        "Byte Type",
+        "Unicode Group",
+    ]);
+
+    table.printstd();
 }
